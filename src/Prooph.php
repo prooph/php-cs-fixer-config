71b386b6eea15fe80326d3759b3d43cc2601c58f
Sandro Keil <github@sandro-keil.de>
2020-12-11 08:25:32 +0100

    Support PHP 8

--- src/Prooph.php
+++ src/Prooph.php
@@ -2,8 +2,8 @@
 
 /**
  * This file is part of `prooph/php-cs-fixer-config`.
- * (c) 2016-2018 prooph software GmbH <contact@prooph.de>
- * (c) 2016-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2016-2020 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2020 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
